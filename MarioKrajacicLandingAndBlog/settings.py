27b168dd1583bc42c2b7e5c0b66d2ba066cbe7cd
Mario Krajacic <krajacic.mario@gmail.com>
2019-09-09 20:19:14 +0200

    Adding users app

--- MarioKrajacicLandingAndBlog/settings.py
+++ MarioKrajacicLandingAndBlog/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'landing.apps.LandingConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
