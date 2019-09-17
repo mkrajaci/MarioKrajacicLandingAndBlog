f44abda3a67c8edb3c1434ce05b36f22eebcd7d4
Mario Krajacic <krajacic.mario@gmail.com>
2019-09-17 06:26:04 +0200

    Adding some profile logic

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 # Ovdje nasljedjujem original forms koji je koristi na admin pageu
 # i dodajem svoja polja koja zelim vidjeti tamo
@@ -12,3 +13,17 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
