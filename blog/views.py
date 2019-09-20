06f4c3164cc2da4585f01674cb15625369e01cb0
Mario Krajacic <krajacic.mario@gmail.com>
2019-09-20 06:22:44 +0200

    Adding class base views

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
@@ -10,6 +11,26 @@ def blog(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/blog.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/blog.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']  # Ovdje mijenjam redosljed postova na blogu
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
